73b01509829aa3a5f2e0a0fe654cf4d6d4ef768b
umbertopollarini <32383505+umbertopollarini@users.noreply.github.com>
2024-10-28 10:58:19 +0100

    Update dependecies.sh

--- dependecies.sh
+++ dependecies.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Aggiorna la lista dei pacchetti
-echo "Updating package list..."
+echo "+ Updating package list..."
 sudo apt update
 
 # Installa le dipendenze principali
-echo "Installing required packages..."
+echo "+ Installing required packages..."
 sudo apt install -y git gpsd gpsd-clients python3-gps python3-pip wireguard resolvconf dnsmasq hostapd i2c-tools
 
 # Installa i pacchetti Python necessari
-echo "Installing Python packages..."
+echo "+ Installing Python packages..."
 pip3 install gpsd-py3 --break-system-packages
 pip3 install psutil --break-system-packages
 pip3 install smbus --break-system-packages
 
 # Verifica l'installazione di gpsd con cgps
-echo "Testing GPSD setup..."
+echo "+ Testing GPSD setup..."
 cgps -s
 
 # Installa tmux per eventuali sessioni persistenti
-echo "Installing tmux..."
+echo "+ Installing tmux..."
 sudo apt install -y tmux
 
-echo "All dependencies have been installed successfully!"
+echo "+ All dependencies have been installed successfully!"
