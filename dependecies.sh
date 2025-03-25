d1030c744573e74a7a0c1855bb0fbff335ca637d
umbertopollarini <32383505+umbertopollarini@users.noreply.github.com>
2025-03-25 11:25:35 +0100

    Update dependecies.sh

--- dependecies.sh
+++ dependecies.sh
@@ -1,73 +1,91 @@
 #!/bin/bash
 
 # Aggiorna la lista dei pacchetti
-echo "Updating package list..."
+echo "----------------------------------------"
+echo "[INFO] Updating package list..."
+echo "----------------------------------------"
 sudo apt update
 
 # Installa le dipendenze principali
-echo "Installing required packages..."
-sudo apt install -y git gpsd gpsd-clients python3-gps python3-pip wireguard resolvconf dnsmasq hostapd i2c-tools libportaudio2
+echo "----------------------------------------"
+echo "[INFO] Installing required packages..."
+echo "----------------------------------------"
+sudo apt install -y git gpsd gpsd-clients python3-gps python3-pip wireguard resolvconf dnsmasq hostapd i2c-tools libportaudio2 tmux
 
 # Setta volume microfono e output
-amixer set Capture 90%
-amixer set Master 90%
+#amixer set Capture 90%
+#amixer set Master 90%
 
 # Installa i pacchetti Python necessari
-echo "Installing Python packages..."
+echo "----------------------------------------"
+echo "[INFO] Installing Python packages..."
+echo "----------------------------------------"
 pip3 install gpsd-py3 --break-system-packages
 pip3 install psutil --break-system-packages
 pip3 install smbus --break-system-packages
 pip3 install sounddevice --break-system-packages
 pip3 install filterpy --break-system-packages
-
 sudo pip3 install mpu6050-raspberrypi --break-system-packages
 sudo pip3 install pyserial --break-system-packages
 sudo pip3 install pynmea2 --break-system-packages
 
-# Installa tmux per eventuali sessioni persistenti
-echo "Installing tmux..."
-sudo apt install -y tmux
+echo "----------------------------------------"
+echo "[INFO] Making Directory..."
+echo "----------------------------------------"
+sudo mkdir logAccGir
+sudo mkdir logRTK
+sudo mkdir logGNSS
 
 # Configura il servizio systemd per horsemonitor
-echo "Setting up horsemonitor service..."
+echo "----------------------------------------"
+echo "[INFO] Setting up horsemonitor service..."
+echo "----------------------------------------"
 sudo cp horsemonitor.service /etc/systemd/system/horsemonitor.service
 sudo systemctl daemon-reload
 sudo systemctl enable horsemonitor.service
 sudo systemctl start horsemonitor.service
 
 # Mostra lo stato del servizio horsemonitor
-echo "Checking horsemonitor service status..."
+echo "----------------------------------------"
+echo "[INFO] Checking horsemonitor service status..."
+echo "----------------------------------------"
 sudo systemctl status horsemonitor.service
 
 # Configura il servizio systemd per receiver_gestionale
-echo "Setting up receiver_gestionale service..."
+echo "----------------------------------------"
+echo "[INFO] Setting up receiver_gestionale service..."
+echo "----------------------------------------"
 sudo cp receiver_gestionale.service /etc/systemd/system/receiver_gestionale.service
 sudo systemctl daemon-reload
 sudo systemctl enable receiver_gestionale.service
 sudo systemctl start receiver_gestionale.service
 
 # Mostra lo stato del servizio receiver_gestionale
-echo "Checking receiver_gestionale service status..."
+echo "----------------------------------------"
+echo "[INFO] Checking receiver_gestionale service status..."
+echo "----------------------------------------"
 sudo systemctl status receiver_gestionale.service
-
-echo "Enabling ssh..."
-sudo systemctl enable ssh
-sudo systemctl start ssh
 
 # Abilito VPN solo se vpn.conf esiste nella directory corrente
 if [ -f "vpn.conf" ]; then
-    echo "Enabling VPN..."
+    echo "----------------------------------------"
+    echo "[INFO] Enabling VPN..."
+    echo "----------------------------------------"
     sudo cp vpn.conf /etc/wireguard/
     sudo wg-quick up vpn
     sudo systemctl enable wg-quick@vpn
     sudo systemctl start wg-quick@vpn
     sudo systemctl status wg-quick@vpn
 else
-    echo "vpn.conf non trovato. Salto la configurazione della VPN."
+    echo "----------------------------------------"
+    echo "[INFO] vpn.conf non trovato. Salto la configurazione della VPN."
+    echo "----------------------------------------"
 fi
 
 # Avvio servizio per acquisizione accellerometri e giroscopi
-echo "Setting up accgir service..."
+echo "----------------------------------------"
+echo "[INFO] Setting up accgir service..."
+echo "----------------------------------------"
 CONFIG_FILE="/boot/firmware/config.txt"
 
 # Aggiunge la riga per abilitare I2C se non già presente
@@ -85,16 +103,15 @@ if ! lsmod | grep -q "^i2c_dev"; then
 fi
 
 sudo timedatectl set-timezone Europe/Rome
-sudo mkdir logAccGir
-sudo mkdir logRTK
 sudo cp accgir.service /etc/systemd/system/accgir.service
 sudo systemctl daemon-reload
 sudo systemctl enable accgir.service
 sudo systemctl start accgir.service
 sudo systemctl status accgir.service
-
-echo "All dependencies and services have been set up successfully!"
+echo "----------------------------------------"
+echo "[COMPLETE] All dependencies and services have been set up successfully!"
 echo "Rebooting in 10 seconds..."
+echo "----------------------------------------"
 
 sleep 10
 sudo reboot
